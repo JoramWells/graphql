dff6b134f4b9d8039b3319859a06ebf9daf74001
bramuel <jorammanoah1@gmail.com>
2021-03-01 14:22:53 -0800

    Another commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const graphqlHTTP = require('express-graphql')
-const schema = require('schema')
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema')
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-    schema:MyGraphQLSchema,
+    schema,
     graphiql:true
 }))
  
