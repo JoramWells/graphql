3c48112a61253710f4eac3ebb7c3607b99e00e85
bramuel <jorammanoah1@gmail.com>
2021-03-02 02:03:42 -0800

    Final commits

--- server.js
+++ server.js
@@ -2,14 +2,24 @@ const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 
 app.use(cors())
+
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:true
 }))
  
-app.listen(3000,()=>{
+app.use(express.static('public/build'))
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'public/build', 'index.html'))
+
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000,()=>{
     console.log('Server running on http://localhost:5000/')
 })
