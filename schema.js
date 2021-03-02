7f7209ebfe111700441ab6391fdda95572e43207
bramuel <jorammanoah1@gmail.com>
2021-03-02 01:34:46 -0800

    Other changes

--- schema.js
+++ schema.js
@@ -7,7 +7,7 @@ const LaunchType = new GraphQLObjectType({
         flight_number:{type:GraphQLInt},
         mission_name:{type:GraphQLString},
         launch_year:{type:GraphQLString},
-        lauch_date:{type:GraphQLString},
+        lauch_date_local:{type:GraphQLString},
         launch_success:{type:GraphQLBoolean},
         rocket:{type:RocketType}
 
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const schema = require('./schema')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql:true
 }))
  
 app.listen(3000,()=>{
-    console.log('Server running on http://localhost:3000/')
+    console.log('Server running on http://localhost:5000/')
 })
